25ace267242baabffc64c92d26798823e38a6656
Alec Tecson <tecsonalr@gmail.com>
2021-05-11 15:33:42 +0800

    Blackbox Testing

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -21,5 +21,35 @@ namespace TestNinja.UnitTests
 
             Assert.That(result, Is.EqualTo(3));
         }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnFirstArgument()
+        {
+             var math = new Math();
+             
+             var result = math.Max(2, 1);
+
+             Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
     }
 }
